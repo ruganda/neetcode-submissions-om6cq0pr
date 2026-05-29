1f2c22dc03764362b90fab2deb9c994512b8d051
Ruganda Mubarak <mubaruganda@gmail.com>
2026-05-29 23:42:35 +0300

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l = 0
+        r = 0
+        n= len(prices)
+        profit = 0
+        while(r<n):
+            diff = prices[r] - prices[l]
+            if diff< 0:
+                l+=1
+                r=l
+            profit = max(diff, profit)
+
+            r+=1
+
+        return profit
